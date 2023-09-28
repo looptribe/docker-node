4759ab517427377c3a9e915dcda92be5e21230b2
Andrea Ghidini <andrea.ghidini@looptribe.com>
2023-09-28 14:50:40 +0200

    Use portainer stack name instead of stack id

--- 18-aws-cli-portainer/portainer-deploy.js
+++ 18-aws-cli-portainer/portainer-deploy.js
@@ -5,7 +5,7 @@ const process = require('process');
 
 const PORTAINER_ACCESS_TOKEN = process.env.PORTAINER_ACCESS_TOKEN;
 const PORTAINER_API_URL = process.env.PORTAINER_API_URL;
-const PORTAINER_STACK_ID = Number(process.env.PORTAINER_STACK_ID) || 0;
+const PORTAINER_STACK_NAME = process.env.PORTAINER_STACK_NAME || '';
 const PORTAINER_STACK_FILE = process.env.PORTAINER_STACK_FILE || 'stack.yml';
 
 const callApi = (url, options = {}) => {
@@ -29,8 +29,8 @@ const portainerPut = (url, data, options = {}) => callApi(url, {
 });
 
 const deploy = async () => {
-    if (!PORTAINER_STACK_ID) {
-        console.error('Environment variable PORTAINER_STACK_ID not set');
+    if (!PORTAINER_STACK_NAME) {
+        console.error('Environment variable PORTAINER_STACK_NAME not set');
         process.exit(1);
     }
     if (!PORTAINER_API_URL) {
@@ -44,6 +44,14 @@ const deploy = async () => {
 
     const stackFile = (await fs.readFile(PORTAINER_STACK_FILE)).toString('utf8');
 
+    console.log('Finding stack...');
+    const stacks = await portainerGet(`/stacks`);
+    const PORTAINER_STACK_ID = stacks.find((s) => s.Name === PORTAINER_STACK_NAME)?.Id;
+    if (!PORTAINER_STACK_ID) {
+        console.error(`Cannot find stack with name "${PORTAINER_STACK_NAME}"`);
+        process.exit(1);
+    }
+    console.log(`Found stack_id=${PORTAINER_STACK_ID}`)
     console.log('Retrieving stack info...');
     const stackInfo = await portainerGet(`/stacks/${PORTAINER_STACK_ID}`);
     console.log('Publishing new stack...');
